13e05f0f2e0c804ffa7e13bbb8ecec536ae5e281
Benjamin Dehli <superelg@gmail.com>
2021-03-08 12:45:16 +0100

    Add actions for authentication and authorization

---
+++ src/actions/AuthenticationActions.js
@@ -0,0 +1,15 @@
+import * as Cookies from 'js-cookie';
+
+export const updateOidcCookie = (user) => dispatch => {
+  if (user) {
+    const accessToken = user.access_token
+      ? user.access_token
+      : null;
+    const expiresAt = user.expires_at
+      ? user.expires_at * 1000
+      : null;
+    if (accessToken && expiresAt) {
+      Cookies.set('oidcAccessToken', accessToken, {expires: new Date(expiresAt)});
+    }
+  }
+};
