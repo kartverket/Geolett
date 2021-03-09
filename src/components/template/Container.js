df2278ac7b6f5446fd65f37123009723ccfbf6b2
Benjamin Dehli <superelg@gmail.com>
2021-03-09 15:02:09 +0100

    Add component for container element

---
+++ src/components/template/Container.js
@@ -0,0 +1,21 @@
+// Dependencies
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import style from 'components/template/Container.module.scss';
+
+// Components
+import Breadcrumbs from 'components/partials/Breadcrumbs';
+
+
+class ContentContainer extends Component {
+   render() {
+      return (
+         <div className={style.container}>
+            <Breadcrumbs />
+            {this.props.children}
+         </div>
+      );
+   }
+}
+
+export default connect(null, null)(ContentContainer);
