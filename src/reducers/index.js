4b2d032d11166d736a4e8dac5ca8a707c195c78b
Benjamin Dehli <superelg@gmail.com>
2021-03-10 14:45:22 +0100

    Add reducer for register items

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,12 +6,14 @@ import { reducer as oidcReducer } from 'redux-oidc';
 // Reducers
 import AuthInfoReducer from 'reducers/AuthInfoReducer';
 import ConfigReducer from 'reducers/ConfigReducer';
+import RegisterItemsReducer from 'reducers/RegisterItemsReducer';
 
 const reducers = history => combineReducers({
   router: connectRouter(history),
   oidc: oidcReducer,
   authInfo: AuthInfoReducer,
-  config: ConfigReducer
+  config: ConfigReducer,
+  registerItems: RegisterItemsReducer
 });
 
 export default reducers
