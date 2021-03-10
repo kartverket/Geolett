c5ba1b44a2e40aba82a136da6d4fea6c8fb06493
Benjamin Dehli <superelg@gmail.com>
2021-03-10 14:46:13 +0100

    Implement fetch method for register items

--- src/components/routes/RegisterItems.js
+++ src/components/routes/RegisterItems.js
@@ -1,6 +1,7 @@
 // Dependencies
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 // Components
 import Container from 'components/template/Container';
@@ -12,10 +13,28 @@ import { fetchRegisterItems } from 'actions/RegisterItemActions';
 
 class RegisterItems extends Component {
 
+   componentDidMount(){
+      this.props.fetchRegisterItems();
+   }
+
+   renderRegisterItems(registerItems) {
+      return registerItems && registerItems.length
+         ? registerItems.map(registerItem => {
+            return (
+               <div>
+                  <Link to={`registerItem/${registerItem.id}`}>
+                     {registerItem.title}
+                  </Link>
+               </div>
+            )
+         }) : '';
+   }
+
    render() {
       return (
          <Container>
             <h1>{this.props.translate('MeasureActivitiesTitle')}</h1>
+            {this.renderRegisterItems(this.props.registerItems)}
          </Container>
       )
    }
