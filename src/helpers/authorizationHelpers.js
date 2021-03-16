7201dea0fb3818590faeaa5ff03c6d636cda762c
Benjamin Dehli <superelg@gmail.com>
2021-03-16 15:06:08 +0100

    Add helpers for authorization

---
+++ src/helpers/authorizationHelpers.js
@@ -0,0 +1,29 @@
+export const hasAdminRole = authInfo => {
+  return authInfo && authInfo.roles && authInfo.roles.includes('nd.metadata_admin');
+}
+
+export const hasEditorRole = authInfo => {
+  return authInfo && authInfo.roles && authInfo.roles.includes('nd.tiltaksplan');
+}
+
+export const isResponsibleAgency = (authInfo = {}, responsibleAgency = {}) => {
+  return parseInt(authInfo.organizationNumber) === responsibleAgency.orgNumber;
+}
+
+export const isOwner = (authInfo = {}, owner = {}) => {
+  return parseInt(authInfo.organizationNumber) === owner.orgNumber;
+}
+
+
+// RegisterItem
+export const canAddRegisterItem = authInfo => {
+  return hasAdminRole(authInfo);
+}
+
+export const canDeleteRegisterItem = authInfo => {
+  return hasAdminRole(authInfo);
+}
+
+export const canEditRegisterItem = authInfo => {
+  return hasAdminRole(authInfo);
+}
