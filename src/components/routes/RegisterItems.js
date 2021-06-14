3374743f8bdf49461737f03a51a7ba9403a3031e
Benjamin Dehli <superelg@gmail.com>
2021-06-14 13:00:34 +0200

    Fix null check on render method for register items

--- src/components/routes/RegisterItems.js
+++ src/components/routes/RegisterItems.js
@@ -16,36 +16,38 @@ import style from 'components/routes/RegisterItems.module.scss'
 
 class RegisterItems extends Component {
 
-   componentDidMount(){
-      this.props.fetchRegisterItems();
+   constructor(props) {
+      super(props);
+      this.state = {
+         registerItemsFetched: false
+      };
+   }
+
+   componentDidMount() {
+      this.props.fetchRegisterItems().then(() => {
+         this.setState({ registerItemsFetched: true });
+      });
    }
 
    renderRegisterItems(registerItems) {
-      const registerItemRows = registerItems && registerItems.length
-         ? registerItems.filter(registerItem => {return registerItem;}).map(registerItem => {
+      const registerItemRows = registerItems?.length
+         ? registerItems.filter(registerItem => { return registerItem; }).map(registerItem => {
             return (<tr key={registerItem.id}>
-                  <td>
+               <td>
                   <Link to={`${process.env.PUBLIC_URL}/${registerItem.id}/`}>
                      {registerItem.contextType}
                   </Link>
-                  </td>
-                  <td>
-                     {registerItem.title}
-                  </td>
-                  <td>
-                     {registerItem.owner && registerItem.owner.name ? registerItem.owner.name : ''}
-                  </td>
-               </tr>)
+               </td>
+               <td>
+                  {registerItem.title}
+               </td>
+               <td>
+                  {registerItem.owner?.name || ''}
+               </td>
+            </tr>)
          }) : null;
-      return registerItemRows ? (<tbody>{registerItemRows}</tbody>) : '';
-   }
-
-   render() {
-      return this.props.registerItems && this.props.registerItems.length ? (
-         <Container>
-            <h1>Konteksttyper</h1>
-            <CreateRegisterItem newRegisterItem />
-
+      return registerItemRows
+         ? (
             <table className={style.registerItemsTable}>
                <thead>
                   <tr>
@@ -54,11 +56,24 @@ class RegisterItems extends Component {
                      <th>Eier</th>
                   </tr>
                </thead>
-               {this.renderRegisterItems(this.props.registerItems)}
+               <tbody>{registerItemRows}</tbody>
             </table>
-            
+         )
+         : '';
+   }
+
+   render() {
+      if (!this.state.registerItemsFetched) {
+         return ''
+      }
+      const registerItems = this.props.registerItems;
+      return (
+         <Container>
+            <h1>Konteksttyper</h1>
+            <CreateRegisterItem newRegisterItem />
+            {this.renderRegisterItems(registerItems)}
          </Container>
-      ) : ''
+      );
    }
 }
 
